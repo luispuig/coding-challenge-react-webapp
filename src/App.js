6aac052576c844f87b706eea74e449995f1c6bcf
luispuig <mail@lpuig.com>
2017-12-06 23:30:18 +0100

    Bootstrap 4 css & menuBar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 type _Props = {};
 
@@ -13,12 +15,21 @@ class App extends Component<_Props> {
     return (
       <Router basename="/">
         <div>
-          <header>
-            <RouteLink to="/"      activeOnlyWhenExact={true} label="Home"/>
-            <RouteLink to="/about" activeOnlyWhenExact={true} label="About"/>
-          </header>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <nav className="bg_color_main navbar navbar-expand-md navbar-dark" >
+            <div className="container">
+              <a className="NavbarBrand" >RectWeather</a>
+              <div className="collapse show navbar-collapse">
+                <ul className="mr-auto navbar-nav">
+                  <RouteLink to="/"      activeOnlyWhenExact={true} label="Home"/>
+                  <RouteLink to="/about" activeOnlyWhenExact={true} label="About"/>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div id="main" className="container">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
       </Router>
     );
@@ -29,12 +40,14 @@ type RouteLink_Props = {
   label: string,
   to: string,
   activeOnlyWhenExact?: boolean,
-}
+};
 
 
 const RouteLink = ({ label, to, activeOnlyWhenExact }:RouteLink_Props) => (
     <Route path={to} exact={activeOnlyWhenExact} children={({ match }:{ match?:boolean }) => (
+        <li className={`nav-item ${ match?'active':'' }`}>
           <Link className='nav-link' to={to}>{label}</Link>
+        </li>
     )}/>
 );
 
