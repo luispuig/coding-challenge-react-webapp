ae254f250450ade1dc2d6cbcc15af33f7218111c
luispuig <mail@lpuig.com>
2017-12-06 23:49:39 +0100

    Revert "City Component: MockUp"
    This reverts commit 811c3711a63b6933052565f365f06d0e1e8fdbad.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,13 @@
 // @flow
+
 import React from 'react'
-import City from './components/City'
 
 type _Props = {};
 
 class Home extends React.Component<_Props> {
   render() {
     return (
-        <div className="container row">
-          <div className="col">
-            <City id={1} state="request" name="Nombre Ciudad" temperature={25}/>
-          </div>
-          <div className="col">
-            <City id={2} state="success" name="Nombre Ciudad" temperature={25}/>
-          </div>
-          <div className="col">
-            <City id={3} state="fail"    name="Nombre Ciudad" />
-          </div>
-          <div className="col">
-            <City id={4} state="fail"    name="Nombre Ciudad" error="error message"/>
-          </div>
-        </div>
+        <div>Home Component!</div>
     );
   }
 }
