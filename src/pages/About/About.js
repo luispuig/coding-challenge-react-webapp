efc9dda52f78fdde7f8566ff7f4897936ce38a71
luispuig <mail@lpuig.com>
2017-12-08 01:28:00 +0100

    About Page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,7 +6,12 @@ type _Props = {};
 class About extends React.Component<_Props> {
   render() {
     return (
-        <div>About Component!</div>
+        <div>
+          <p>ReactJS Test made by Luis Puig</p>
+          <p><b>Repository: </b><a href="https://github.com/luispuig/altran/" target="_blank">https://github.com/luispuig/altran/</a></p>
+          <p><b>Test coverage report (100%): </b><a href="https://luispuig.com/altran/test/" target="_blank">https://luispuig.com/altran/test/</a></p>
+          <p><b>Flow coverage report (99%): </b><a href="https://luispuig.com/altran/flow/" target="_blank">https://luispuig.com/altran/flow/</a></p>
+        </div>
     );
   }
 }
