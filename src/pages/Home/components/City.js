94ce753339b6103c767505931709683c7a89ab6b
Luis Puig <mail@lpuig.com>
2019-01-11 15:15:42 +0100

    Fix formatDate

--- src/pages/Home/components/City.js
+++ src/pages/Home/components/City.js
@@ -1,36 +1,37 @@
 // @flow
 import * as React from "react";
 
-import Spinner  from "react-activity/lib/Spinner/";
+import Spinner from "react-activity/lib/Spinner/";
 import "react-activity/lib/Spinner/Spinner.css";
 
-import type { _City } from '../../../redux/cities/types';
-import type { _Temperature } from '../../../redux/cities_temperatures/types';
+import type { _City } from "../../../redux/cities/types";
+import type { _Temperature } from "../../../redux/cities_temperatures/types";
 
 import "./City.css";
 
 type _Props = {
   ..._City,
   temperatures: Array<_Temperature>
-}
+};
 
 class City extends React.Component<_Props> {
-
   _renderTemperature = () => {
-    const {state, temperature} = this.props;
-    switch(state) {
+    const { state, temperature } = this.props;
+    switch (state) {
       case "request": // Loading
         return <Spinner />;
 
       case "success":
-        if(temperature === undefined) return null;
-        return ( <div>{ Math.round(temperature) }
-              &nbsp;<span className="unit">ºC</span>
-            </div>
+        if (temperature === undefined) return null;
+        return (
+          <div>
+            {Math.round(temperature)}
+            &nbsp;<span className="unit">ºC</span>
+          </div>
         );
 
       case "fail":
-        return <div className="error">X</div> ;
+        return <div className="error">X</div>;
 
       default:
         return null;
@@ -39,9 +40,10 @@ class City extends React.Component<_Props> {
 
   // Aux function to format date dd/MM/yyyy HH:mm
   // TODO IMPROVE Move function to utils file
-  _formatDate = (date:Date) => {
+  formatDate = (date: Date) => {
+    console.log(date);
     const dd = ("00" + date.getDate()).slice(-2);
-    const mm = ("00" + date.getMonth()).slice(-2);
+    const mm = ("00" + (date.getMonth() + 1)).slice(-2);
     const yyyy = date.getFullYear();
     const _hh = ("00" + date.getHours()).slice(-2);
     const _mm = ("00" + date.getMinutes()).slice(-2);
@@ -52,39 +54,35 @@ class City extends React.Component<_Props> {
   render() {
     const { name, state, error, temperatures } = this.props;
     return (
-        <div className="_City card elevation-1 elevation-hover">
-          <div className="card-body card-img-top bg_color_main _CardBody">
-            <div className="frame-temperature">
-              <div className="temperature elevation-2 text_color_dark">
-                { this._renderTemperature() }
-              </div>
+      <div className="_City card elevation-1 elevation-hover">
+        <div className="card-body card-img-top bg_color_main _CardBody">
+          <div className="frame-temperature">
+            <div className="temperature elevation-2 text_color_dark">
+              {this._renderTemperature()}
             </div>
-            <div className="card-title">{name}</div>
           </div>
-          <div className="list-group list-group-flush list-temperatures">
-            {
-              // Check and show error message
-              (state === "fail") && error &&
-              <li className="list-group-item error">
-                {error}
-              </li>
-            }
-            {
-              temperatures.map( (item:_Temperature, index:number) =>
-                  <li key={index} className="list-group-item">
-                    <div className="row">
-                      <div className="col-7 col-md-8">
-                        {this._formatDate(item.date)}
-                      </div>
-                      <div className="col-5 col-md-4 text-right">
-                        { Math.round(item.temperature) }
-                        &nbsp;<span className="unit">ºC</span></div>
-                    </div>
-                  </li>
-              )
-            }
-          </div>
+          <div className="card-title">{name}</div>
         </div>
+        <div className="list-group list-group-flush list-temperatures">
+          {// Check and show error message
+          state === "fail" && error && (
+            <li className="list-group-item error">{error}</li>
+          )}
+          {temperatures.map((item: _Temperature, index: number) => (
+            <li key={index} className="list-group-item">
+              <div className="row">
+                <div className="col-7 col-md-8">
+                  {this.formatDate(item.date)}
+                </div>
+                <div className="col-5 col-md-4 text-right">
+                  {Math.round(item.temperature)}
+                  &nbsp;<span className="unit">ºC</span>
+                </div>
+              </div>
+            </li>
+          ))}
+        </div>
+      </div>
     );
   }
 }
